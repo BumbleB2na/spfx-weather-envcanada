43054bfbf79740842a0c9506a085283c756171eb
b2 <brendan@mobilewebsmart.com>
2020-03-09 09:21:31 -0600

    yo @microsoft/sharepoint

---
+++ src/webparts/weatherEnvironmentCanada/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IWeatherEnvironmentCanadaWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'WeatherEnvironmentCanadaWebPartStrings' {
+  const strings: IWeatherEnvironmentCanadaWebPartStrings;
+  export = strings;
+}
